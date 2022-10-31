10d70595d97134ef1cd0de8e752516966d137112
Jeaustin <JeaustinS27@gmail.com>
2022-10-31 03:19:04 -0400

    created prompt to choose desired length of password and began generating random password dependant on selections

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,40 @@ var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var finalPW = []
+
 
 function generatePassword() {
-  return "generated password";
+    // userLength()
+    var pwLength = prompt("How long would you like your password to be? Must be between 8 and 128 characters.");
+    console.log(pwLength)
+
+    if (pwLength < 8 || pwLength > 128) {
+        alert("Number entered was not within the specified requirements. Please try again.");
+        return "Try Again"
+    } else if (isNaN(pwLength)) {
+        alert("Value entered is not a number. Please try again.");
+        return "Try Again"
+    } else
+    var pwUpperC = confirm("Would you like Upper Case letters in your password? OK = Yes CANCEL = NO");
+    console.log(pwUpperC)
+    var pwLowerC = confirm("Would you like Lower Case letters in your password? OK = Yes CANCEL = NO");
+    console.log(pwLowerC)
+    var pwNumbers = confirm("Would you like numbers in your password? OK = Yes CANCEL = NO");
+    console.log(pwNumbers)
+    var pwSpecial = confirm("Would you like special characters in your password? OK = Yes CANCEL = NO");
+    console.log(pwSpecial)
+
+    if (!pwUpperC && !pwLowerC && !pwNumbers && !pwSpecial) {
+        alert("No specifications were selected. Please select at least one.");
+        return "Try Again";
+    } else if (pwUpperC && pwLowerC && pwNumbers && pwSpecial) {
+        var Characters = upperC.concat(lowerC, numbers, special);
+        
+
+    }
+    
+//   return "generated password";
 }
 
 // Write password to the #password input
